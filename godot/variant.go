42a377ca3d65f66a30e2f209e774aee4872bde06
William Edwards <shadowapex@gmail.com>
2017-08-19 12:08:31 -0700

    Clean up variant code

--- godot/variant.go
+++ godot/variant.go
@@ -45,19 +45,3 @@ const (
 	VariantTypePoolVector3Array
 	VariantTypePoolColorArray
 )
-
-func NewVariant() *Variant {
-	return &Variant{}
-}
-
-type Variant struct {
-}
-
-func (v *Variant) Value() C.godot_variant {
-	var godot_variant C.godot_variant
-	if v == nil {
-		C.godot_variant_new_nil(&godot_variant)
-	}
-
-	return godot_variant
-}
